042aa28dc1ab366ded2623f6290c8d6c5f68aed0
Donald Udeh <somtoudehd@gmail.com>
2026-04-29 21:21:15 -0400

    Final Game - Final Version of run.cpp

--- Simon-says/src/run.cpp
+++ Simon-says/src/run.cpp
@@ -1,3 +1,5 @@
+
+
 #include <Arduino.h>
 #include <func.h>
 #include <btn.h>
@@ -7,8 +9,9 @@
 #define PRESCALER 64
 #define FREQ F_CPU/PRESCALER
 // red = 0, yellow = 3, goes r -> g -> b -> y
+// red is segA 
 
-#define numDigits 5
+#define numDigits 6
 #define numColors 4
 #define numBtns 4
 #define colorIndex 4
@@ -19,12 +22,13 @@
 #define blueBtn     2
 #define yellowBtn   3
 #define buzzerPin   10
-#define oePin       4   // ! make sure to change PORTs if change
+#define oePin       5   // ! make sure to change PORTs if change
 #define latchPin    7   // !
 #define clockPin    11
 #define dataPin     12
+#define randomSeedPin   A1
 #define potPin      A0
-#define brokenPins  6, 8, 9
+// #define brokenPins  6, 8, 9     // broken legs of my atmega328p dip chip
 
 // tone stuff
 #define ONE_SECOND_MS 1000
@@ -33,7 +37,14 @@
 #define numSuccessNotes 4   // ! remember to change if change
 #define numFailureNotes 3   // !
 
-#define ADMUXSetupSettings (1 << REFS1) | (1 << REFS0)  /*use internal 1.1v as referece */ | (1 << ADLAR) // make it left adjusted, so that we only grab ADCH 
+#define ADMUXSetupSettings (1 << REFS0)  /*use Avcc as reference */ | (1 << ADLAR) // make it left adjusted, so that we only grab ADCH 
+
+// #define stuff for loop, defined because not changing
+#define aReadDelay 100
+#define ledOffDelay 100
+#define noteOnDelay 100
+
+
 
 void updateCurrentScore();
 void setupTimer2();
@@ -42,6 +53,8 @@ void myTone(u16 note);
 void endMyTone();
 void clearPresses();
 void setupADCandPin();
+void tagAlongTimer0();
+void updateHighScore();
 
 u8 colorArray[100] = {0};
 u8 currentStreak = 0;
@@ -59,25 +72,19 @@ u8 successDuration[] = {8, 8, 8, 4};
 my595 sRegs = {dataPin, clockPin, latchPin};
 
 
-enum activity {addLed, showingPattern, readingBtns, levelPassed, levelFailed};
+enum activity {addLed, showingPattern, readingBtns, levelPassed, levelFailed, celebration};
 activity currentActivity = addLed;
 
 
 // For display
-// 0-1 is Highscore, 2-3 is current streak, 4 is led to display
-volatile u8 displayBuffer[5] = {0};
+// 0-1 is Highscore, 2-3 is current streak, 4 is led to display, 5 is onProgressLeds
+volatile u8 displayBuffer[numDigits] = {0};
 volatile u8 currentDigit = 0;
 volatile const uint8_t digitsPattern[10] =  {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
-volatile u8 brightness = 255;
-volatile u8 numTimesVisitedInterrupt = 0;
+volatile u8 highScore;
 
 // 244Hz
 ISR(TIMER2_OVF_vect){
-    if (numTimesVisitedInterrupt == brightness){
-        PORTD |= (1 << oePin);  // active low, so sending HIGH connects to GND
-    } else if (numTimesVisitedInterrupt == 0){
-        PORTD &= ~(1 << oePin);
-    }
     PORTD &= ~(1 << latchPin); 
     msbShiftOut(sRegs, ~(1 << currentDigit)); 
     msbShiftOut(sRegs, displayBuffer[currentDigit]); 
@@ -85,14 +92,14 @@ ISR(TIMER2_OVF_vect){
     
     currentDigit++;
     if (currentDigit >= numDigits) currentDigit = 0;
-    numTimesVisitedInterrupt++;
 }
 
 void setup(){
+    randomSeed(analogRead(A1));
     setupTimer2();
     setupTimer1();
-    setupADCandPin();
-    randomSeed(analogRead(A0));
+    setupADCandPin(); 
+    tagAlongTimer0();
 
     pinMode(oePin, OUTPUT);
     pinMode(latchPin, OUTPUT);
@@ -102,13 +109,12 @@ void setup(){
     dWrite(buzzerPin, LOW);
 }
 
-#define aReadDelay 100
 u32 aReadTS = millis();
+
+u8 onProgressLeds = 0;
 
 u8 onLed = 0;
 u32 lastLedTS = millis();
-// ! storing as a macro to save space, will move to the top later
-#define ledOffDelay 100
 u8 ledOnDelay = 255;
 bool ledIsOn = false;
 
@@ -120,9 +126,10 @@ u8 indexToEnter;
 u8 toneIndex = 0;
 bool notePlaying;
 u16 noteOffDelay;
-#define noteOnDelay 100
 u32 noteTS = millis();
 u8 correctLed;      // the led that the user was supposed to press
+
+
 
 void loop(){
     u32 now = millis();     // avoid calling millis() 15 times per loop becuase i have no delay() and no blocks, difference in accuracy very small
@@ -130,7 +137,7 @@ void loop(){
         colorBtns[i].watch(now);
     }
     if ((now - aReadTS > aReadDelay) && ((ADCSRA & (1 << ADSC)) == 0) /*becomes 0 when conversion complete*/ ){
-        brightness = ADCH;  // High part, left adjusted
+        OCR0B = ADCH;  // High part, left adjusted
         aReadTS = now;
         ADCSRA |= (1 << ADSC);  // start conversion 
     }
@@ -140,13 +147,23 @@ void loop(){
             colorArray[currentStreak] = randomLed;
             currentStreak++;
             updateCurrentScore();
+            if (currentStreak % 5 == 0){    // no risk of currentStreak == 0 at this point
+                onProgressLeds = (onProgressLeds << 1 /*double*/) + 1; // every 5 currentStreaks, turn on one more LED, 0 -> 1 -> 11 -> 111, remember LSB is SegA, first physical LED from left to right 
+                displayBuffer[5] = onProgressLeds;
+                ledOnDelay -= 50;
+                // current logic assumes max streak < 20, so no risk of u8 underflow
+            }
+            if (currentStreak > highScore){
+                highScore = currentStreak;
+                updateHighScore();
+            }
             currentActivity = showingPattern;
             onLed = 0;
             ledIsOn = false;
             btnPressedEvent = false;
             flashedPressedLed = false;
             lastLedTS = now;   // Because the next state needs it to be refreshed
-            // Because this case runs every time 
+            // this case runs once every currentStreak
         }   break;
         case showingPattern:{
             if (onLed == currentStreak){
@@ -259,6 +276,9 @@ void loop(){
                 toneIndex = 0;
                 currentStreak = 0;
                 flashedPressedLed = false;
+                onProgressLeds = 0;
+                displayBuffer[5] = onProgressLeds;
+                ledOnDelay = 255;
             }
             else{
                 if (!notePlaying && now - noteTS > noteOnDelay){
@@ -277,8 +297,9 @@ void loop(){
                 }
             }
         }   break;
+
     }
-// atp, im just resetting every variable at each case
+// at this point, im just resetting every variable at each case
 }
 
 void updateCurrentScore(){
@@ -288,7 +309,7 @@ void updateCurrentScore(){
     else{
         lastUpdatedNum = currentStreak;
         u8 firstDigit = currentStreak/10;
-        u8 secondDigit = currentStreak - (firstDigit * 10);  // minus faster than modulus
+        u8 secondDigit = currentStreak % 10;
 
         // NOTE: can be removed so that 05 shows, instead of just 5
         if (firstDigit == 0){
@@ -301,10 +322,25 @@ void updateCurrentScore(){
     }
 }
 
+
+
+void updateHighScore(){
+    u8 firstDigit = highScore/10;
+    u8 secondDigit = highScore % 10;
+
+    // NOTE: can be removed so that 05 shows, instead of just 5
+    if (firstDigit == 0){
+        displayBuffer[2] = 0;
+    }
+    else{
+        displayBuffer[0] = digitsPattern[firstDigit];
+    }
+    displayBuffer[1] = digitsPattern[secondDigit] | (1 << 7) /*Enable the colon seperator*/;
+}
+
 void setupTimer2(){
     cli();
     TCCR2A = 0; 
-    // TCCR2B = (1 << CS22); // 64 PRESCALER (Fires ~976 times/sec)
     TCCR2B = (1 << CS22) | (1 << CS21); // 256 PRESCALER (Fires ~244 times/sec)
     TIMSK2 = (1 << TOIE2); 
     sei();
@@ -313,22 +349,18 @@ void setupTimer2(){
 void setupTimer1(){
     cli();
 
-    // TCCR1A = 0;     // CTC mode
     TCCR1A = (1 << COM1B0);   // Toggle OC1B on compare match, which is what we want
     TCCR1B = (1 << WGM12);  // CTC with OCR1A as top
-    // TCCR1B |= (1 << CS10);    // 1 PRESCALER
     TCCR1B |= (1 << CS11) | (1 << CS10);    // 64 PRESCALER
     // TIMSK1 = (1 << OCIE1A);     // Enable compare match A  . commenting cause the cpu is not involved anymore
     OCR1A = 65535;      // starting value
     OCR1B = 65535;      // has to be less than or equal to OCR1A
-
     sei();
-
 }
 
 void myTone(u16 note){  
     // Timer1 best bet is 64 PRESCALER
-    TCNT1 = 0;   // reset the timer so that i can start counting up to OCR1A immediately
+    TCNT1 = 0;   // reset the timer so that i can start counting up to OCR1A immediately, very important
     OCR1A = (FREQ / (2 * note)) - 1;    // 0 to OCR1A - 1 = OCR1A steps
     OCR1B = OCR1A;
     TCCR1A |= (1 << COM1B0);    //  Enable the toggle OC1B on compare match
@@ -348,101 +380,13 @@ void clearPresses(){
 void setupADCandPin(){
     ADMUX = ADMUXSetupSettings | (potPin - A0); // which pin to watch
     ADCSRA = (1 << ADPS2);  // 16 prescaler no need for high accuracy
+    ADCSRA |= (1 << ADEN);  // enable conversion
     ADCSRA |= (1 << ADSC);  // have to write to one to start conversion, no need to do it here, but just do it
 }
 
-// /* Bug Log:
-//     Using a ISR frequency of 62500 Hz caused loop() to not run 
-//         fix -> Changed to PRESCALER of 256. I used 62500Hz so i could use the same vector for other things but it didnt give brain to other functions
-
-//     Using tone broke my logic completely, caused confusing unrelated bugs like the display blinking currentStreak times, instead of the Leds, this is because tone also uses timer2
-//         fix ->  Going to create a myTone() function, that is only "closely" accurate for the notes used in this game, which are smaller notes, so i dont have to have a very high resolution
-
-
-//     The loop was very responsive, causing it to store button presses and when it was read, i read the previous button presses and it messed up the logic. very sneaky bug
-//         fix -> I cleared all the buttons before the next round
-
-//     The tone worked, but it sounded grainy, which was most likely due to the ISR in Timer2, when we tried to generate a tone, the two most likely tried to clash with each other
-//         fix -> changed from a software interrupt to a hardware interrupt, instead of the cpu running a task or waiting for timer2's ISR to finish, it immediately toggles the pin
-//         Could have used OC1A which is Digital pin 9, but my pin 9 on my atmega328p chip is broken, so i used OC1B (on Digital pin 10), and switched latchPin to DP8(PB0)
-
-//     // ! Always check pinMode(), produces annoying bugs
-//     // ! Always check GND connections
-// */
-
-// #include <arduino.h>
-
-// u8 aRead(u8 pin);
-
-// volatile u8 result;
-// #define  ADMUXSetupSettings  (1 << REFS1) | (1 << REFS0)  /*use internal 1.1v as referece */ | (1 << ADLAR) // make it left adjusted, so that we only grab ADCH 
-
-// void setup(){
-//     ADMUX = ADMUXSetupSettings;
-//     // ADCSRA = (1 << ADPS2);      // 16 prescaler
-//     ADCSRA = (1 << ADPS0);      // 2 prescaler
-//     Serial.begin(9600);
-    
-
-// }
-
-// void loop(){
-//     Serial.println(aRead(A0));
-//     delay(100);
-// }
-
-// u8 aRead(u8 pin){
-//     if (pin > A5) return 0;
-//     ADMUX = ADMUXSetupSettings | (pin - A0);
-    
-//     ADCSRA |= (1 << ADEN);  // turn it on
-    
-//     ADCSRA |= (1 << ADSC);  // have to write to one to start conversion
-    
-//     // 3 methods
-//     while (ADCSRA & (1 << ADSC)); // loop until ADSC bit becomes 0
-//     // ADCSRA &= ~(1 << ADEN);  // turn it off when done no need to turn off 
-//     return ADCH;
-//     // // or replace with
-//     // ADCSRA |= (1 << ADIE);   // ADC enable interrupt
-
-// }
-// // ISR(ADC_vect){
-// //     result = ADCH;
-// // }
-
-
-/*
-    Analog input channel i.e Aref or Avcc is selected by writing to the MUX buts in ADMUX    
-    Enable ADC by setting the ADC enable bit, ADEN in ADCSRA
-    ADEN MUST be set, 
-    ADC doesnt consume power when ADEN is cleared 
-
-    ADC returns the 10 bit result in ADCH and ADCL, with bit 8-9 in ADCH, and 0-7 in ADCL, can be changed bty setting the ADLAR in ADMUX, but we dont care abt that 
-    NOTE: thinking of making it left adjusted tho, since i dont need crazy precision (only 8 bits), and just read ADCH like the datasheet says, but i have to make it left adjusted 
-    0000 0011 1000 1000 which is the normal 10-bit result with a decimal value of 904 becomes 1110 0010 which is 226 in decimal, 904/1024 = 226/256
-
-    Dont think this matters to me but if right adjusted, we have to read ADCL first 
-    
-    we start a conversion(reading) by disabling the power reduction ADC bit PRADC, and enabling the ADC start conversion bit ADSC, ADSC lives in ADCSRA but PRADC is somewhere else
-    NOTE: check if we need to disable PRADC, or if its already disabled
-    ADSC stays HIGH as long as conversion is happening,  and is cleared by hardware when fisnished, it will finish the current conversion before moving to a new channel if one happens to be selected mid conversion
-
-    Auto triggering?
-        enabled by enabling the ADATE bit in ADCSRA
-        trigger source selected by choosing which ADTS2:0 bits are on in ADCSRB, dont think ill need it but seems cool, can be used to start conversion on certain timer overflows or compares etc
-        a conversion can be triggered without an interrupt, so just the flag needed, but u need to clear it to detect another positive edge
-        go to read this parrt again to understand fully
-    
-    
-    NOTE: By degault, approximately 50 to 200kHz is needed to get max resolution, but if the lower than 10 bits is needed, the input clock freq can be higher than 200kHz to get a higher sample rate, will most likely do
-
-    Prescaling is set by the ADPS bits in ADCSRA. Able to generate any cpu freq above 100kHz (any?.. what abt 300MHz 🤨)
-    prescaler runs as long as ADEN is HIGH, resets when ADEN is LOW
-
-    normal conversion takes 13 ADC clock cycles, wonder how many ADC = CPU. Takes 25 the first time to actually initialize internal stuff
-
-    the internal 1.1v takes sometime to stabilize, so the first read might be wwrong
-
-
-*/
+void tagAlongTimer0(){
+    DDRD |= ( 1 << oePin);      // set pin 5 as output
+    TCCR0A |= (1 << COM0B1) | (1 << COM0B0);    // fast pwm mode, inverting mode, enable OC0B inverting mode because oe is active low
+    // OCR0B doesnt have to be > OCR0A in fast PWM, they are independednt of each other
+    // Only reason i used OCR0A instead of OCR0B is because pin 6 is broken
+}
